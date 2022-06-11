1f2f2ef5bfbf11fa8e612eb0f4059aad30af0d37
Kirill Tokarev <kirill.tokarev@osinit.com>
2022-06-11 19:18:31 +0300

    add graph for perticipants

--- src/pages/Analysis.jsx
+++ src/pages/Analysis.jsx
@@ -19,6 +19,7 @@ import {
 function Analysis() {
   const [sport, setSport] = useState(null);
   const [chartData, setData] = useState(null);
+  const [chartData2, setData2] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const { types, events, searchValue, handleChange, isEventLoading } =
@@ -27,6 +28,7 @@ function Analysis() {
   useEffect(() => {
     if (!searchValue) return;
     setData(null);
+    setData2(null);
     setIsLoading(true);
     getSportInfo(searchValue.pk)
         .then(({ data }) => {
@@ -44,8 +46,15 @@ function Analysis() {
             { name: '2020', value: sport.count_events_in_year[2] },
             { name: '2019', value: sport.count_events_in_year[3] },
             { name: '2018', value: sport.count_events_in_year[4] },
-          ].reverse(),
-      );
+          ].reverse());
+      setData2(
+          [
+            { name: '2022', value: sport.avg_count_participant_in_year[0] },
+            { name: '2021', value: sport.avg_count_participant_in_year[1] },
+            { name: '2020', value: sport.avg_count_participant_in_year[2] },
+            { name: '2019', value: sport.avg_count_participant_in_year[3] },
+            { name: '2018', value: sport.avg_count_participant_in_year[4] },
+          ].reverse());
     }
   }, [sport]);
 
@@ -59,7 +68,9 @@ function Analysis() {
         </div>
       )}
       {searchValue ? (
-        <div>
+        <div style={{display: 'flex', flexDirection: 'column', alignContent: 'center', width: '100%',
+          position: 'absolute',
+          left: 0}}>
           <div className={styles.eventTitle}>
             {searchValue.name.toUpperCase()}
           </div>
@@ -73,56 +84,110 @@ function Analysis() {
                 <CircularProgress />
               </div>
             ) : chartData ? (
-              <div>
-                <div className={styles.analSubTitle}>
+              <div className={styles.analGraphs}>
+                <div>
+                  <div className={styles.analSubTitle}>
                   Количество событий всего: {sport.count_events}
-                </div>
-                <div className={styles.analSubTitle}>
+                  </div>
+                  <div className={styles.analSubTitle}>
                   Количество событий за текущий год:{' '}
-                  {sport.count_events_cur_year}
+                    {sport.count_events_cur_year}
+                  </div>
+                  <div
+                    style={{ width: '700px', height: '500px', margin: 'auto' }}
+                  >
+                    <ResponsiveContainer width='100%' height='100%'>
+                      <BarChart
+                        width={500}
+                        height={150}
+                        data={chartData}
+                        margin={{
+                          top: 5,
+                          right: 30,
+                          left: 20,
+                          bottom: 5,
+                        }}
+                      >
+                        <CartesianGrid strokeDasharray='3 3' />
+                        <XAxis
+                          label={{
+                            value: 'Год',
+                            offset: -2,
+                            position: 'insideBottom',
+                          }}
+                          name='Количество событий'
+                          dataKey='name'
+                        />
+                        <YAxis
+                          label={{
+                            value: 'Количество событий',
+                            angle: -90,
+                            position: 'insideLeft',
+                          }}
+                        />
+                        <Tooltip />
+                        <Legend />
+                        <Bar
+                          offset='100px'
+                          name='Количество событий'
+                          dataKey='value'
+                          fill='#8884d8'
+                        />
+                      </BarChart>
+                    </ResponsiveContainer>
+                  </div>
                 </div>
-                <div
-                  style={{ width: '700px', height: '500px', margin: 'auto' }}
-                >
-                  <ResponsiveContainer width='100%' height='100%'>
-                    <BarChart
-                      width={500}
-                      height={150}
-                      data={chartData}
-                      margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                      }}
-                    >
-                      <CartesianGrid strokeDasharray='3 3' />
-                      <XAxis
-                        label={{
-                          value: 'Год',
-                          offset: -2,
-                          position: 'insideBottom',
+                <div>
+                  <div className={styles.analSubTitle}>
+                  Среднее количество участников за текущий год:{' '}
+                    {sport.avg_participants_last_year}
+                  </div>
+                  <div className={styles.analSubTitle}>
+                    &nbsp;
+                  </div>
+                  <div
+                    style={{ width: '700px', height: '500px', margin: 'auto' }}
+                  >
+                    <ResponsiveContainer width='100%' height='100%'>
+                      <BarChart
+                        width={500}
+                        height={150}
+                        data={chartData2}
+                        margin={{
+                          top: 5,
+                          right: 30,
+                          left: 20,
+                          bottom: 5,
                         }}
-                        name='Количество событий'
-                        dataKey='name'
-                      />
-                      <YAxis
-                        label={{
-                          value: 'Количество событий',
-                          angle: -90,
-                          position: 'insideLeft',
-                        }}
-                      />
-                      <Tooltip />
-                      <Legend />
-                      <Bar
-                        offset='100px'
-                        name='Количество событий'
-                        dataKey='value'
-                        fill='#8884d8'
-                      />
-                    </BarChart>
-                  </ResponsiveContainer>
+                      >
+                        <CartesianGrid strokeDasharray='3 3' />
+                        <XAxis
+                          label={{
+                            value: 'Год',
+                            offset: -2,
+                            position: 'insideBottom',
+                          }}
+                          name='Количество участников'
+                          dataKey='name'
+                        />
+                        <YAxis
+                          label={{
+                            value: 'Количество участников',
+                            angle: -90,
+                            position: 'insideLeft',
+                          }}
+                        />
+                        <Tooltip />
+                        <Legend />
+                        <Bar
+                          offset='100px'
+                          name='Количество участников'
+                          dataKey='value'
+                          fill='#8884d8'
+                        />
+                      </BarChart>
+                    </ResponsiveContainer>
+                  </div>
                 </div>
               </div>
             ) : (
